27cfdabc3a320a42df1f36d8898e3f1c1316ac40
TDuffin <thomaslukeduffin@hotmail.co.uk>
2018-10-25 11:24:24 +0100

    First commit

---
+++ Monogame Project 1/Monogame Project 1/Class1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame_Project_1
+{
+    public abstract class Mob
+    /*
+     The "Mob" class contains some boilerplate code and data for all moving objects (mobiles) such as
+     players, enemies, projectives, moving platforms, which can be shared between ALL mobs.
+
+    NB: The name "mob" pays homage to the Minecraft term "mobs", a name that referred to any non-player
+    active entity in the game. couldn't keep it as Mobiles!
+     */
+    {
+        // Some basic shared info for all mobs -- Players, enemies, moving platforms, etc.
+        protected Texture2D Tex;
+        public Vector2 Position, Velocity;
+        public float Width, Height;
+
+        // Collision code will be useful for all mobs, might just make this an external tool later.
+        // All collisions, at least for now, are basic AABB collision detections. Pixel Masks would be ideal.
+        // Can accept any Mob class type. Again, might be work externalising this for statics for a platformer game...
+        public bool CollidingWith(Mob Mobile)
+        {
+            // AABB Collision proves true if other mobile and THIS mobile intersect
+            if (Mobile.Position.X < this.Position.X + this.Width &&
+                Mobile.Position.X + Mobile.Width > this.Position.X &&
+                Mobile.Position.Y < this.Position.Y + this.Height &&
+                Mobile.Position.Y + Mobile.Height > this.Position.Y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Update mob with delta time -- Abstract
+        public abstract void Update(float Delta_time);
+
+        // Draw mob using spritebatches -- Abstract
+        public abstract void Draw(SpriteBatch SpriteBatch);
+    }
+
+    
+     
+    // PLAYER CLASS -- Just encapsulating the earlier code into a class
+    public class Player : Mob
+    {
+        private float Speed = 400f;
+
+        public Player(Vector2 pos, Texture2D tex)
+        {
+            this.Position = pos;
+            this.Velocity = Vector2.Zero;
+            this.Tex = tex;
+
+            this.Width = this.Tex.Width;
+            this.Height = this.Tex.Height;
+        }
+
+        public override void Update(float Delta_time)
+        {
+
+            this.Velocity = new Vector2(0, 0);
+            KeyboardState kstate = Keyboard.GetState();
+            if (kstate.IsKeyDown(Keys.W))
+            { this.Velocity += new Vector2(0, -1); }
+            //if (kstate.IsKeyDown(Keys.A))
+            //{ plrVel += new Vector2(-1, 0); }
+            if (kstate.IsKeyDown(Keys.S))
+            { this.Velocity += new Vector2(0, 1); }
+            //if (kstate.IsKeyDown(Keys.D))
+            //{ plrVel += new Vector2(1, 0); }     
+
+            this.Position += this.Velocity * Speed * (Delta_time);
+            
+        }
+        public override void Draw(SpriteBatch SpriteBatch)
+        {
+            SpriteBatch.Draw(this.Tex, this.Position, Color.White);
+        }
+    }
+
+    public class Ball : Mob
+    {
+        public int axis = 0;
+
+        public Ball(Vector2 pos, Vector2 startvel, Texture2D tex)
+        {
+            this.Position = pos;
+            this.Velocity = startvel;
+            this.Tex = tex;
+
+            this.Width = this.Tex.Width;
+            this.Height = this.Tex.Height;
+
+        }
+
+        public override void Update(float Delta_time)
+        {
+            Vector2 dir;
+
+            switch (this.axis)
+            {
+                case 0:
+                    dir = new Vector2(1, 0);
+                    break;
+                case 1:
+                    dir = new Vector2(0, 1);
+                    break;
+                default:
+                    dir = new Vector2(0, 0);
+                    break;                
+            }
+
+            this.Position += this.Velocity * dir * (Delta_time);
+
+        }
+
+        public override void Draw(SpriteBatch SpriteBatch)
+        {
+            SpriteBatch.Draw(this.Tex, this.Position, Color.White);
+        }
+    }
+
+
+}
---
+++ Monogame Project 1/Monogame Project 1/Game1.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Monogame_Project_1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D plrSprite;
+        Texture2D ballSprite;
+
+        float w, h;
+        
+        Ball ball;
+        Player player;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            w = (float)GraphicsDevice.Viewport.Width;
+            h = (float)GraphicsDevice.Viewport.Height;
+
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            plrSprite = Content.Load<Texture2D>("Paddle");
+            ballSprite = Content.Load<Texture2D>("Player");
+
+            // Using ClassMode to ignore old code (but I want to make sure new code works!)
+            ball = new Ball(new Vector2(w/2, h/2), new Vector2(-400, -300), ballSprite);
+            player = new Player(new Vector2(30, h / 2), plrSprite);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // Get the keyboad state
+            KeyboardState kstate = Keyboard.GetState();
+
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            player.Update(dt);
+
+            // Update based on gametime
+            ball.axis = 0;
+            ball.Update(dt);
+
+            // Ball Collision X code
+            if (ball.Position.X < 0)
+            {
+                ball.Position.X = w - ball.Width;
+            }
+
+            if (ball.Position.X + ball.Width > w)
+            {
+                ball.Velocity.X *= -1;
+            }
+
+            if (ball.CollidingWith(player))
+            {
+                ball.Velocity.X *= -1;
+                ball.Position.X = player.Position.X + player.Width;
+            }        
+
+            // Move in y axis then check for cols again
+            ball.axis = 1;
+            ball.Update(dt);
+
+            if (ball.CollidingWith(player))
+            {
+               
+                if (ball.Velocity.Y < 0)
+                { ball.Position.Y = player.Position.Y + player.Height; }
+                else
+                { ball.Position.Y = player.Position.Y - ball.Height; }
+
+                ball.Velocity.Y *= -1;
+            }
+
+            if (ball.Position.Y <= 0 || ball.Position.Y + ball.Height >= h)
+            { ball.Velocity.Y *= -1; }
+
+            if (player.Position.Y + player.Height > h)
+            {
+                player.Position.Y = h - player.Height;
+            }
+            else if (player.Position.Y < 0)
+            {
+                player.Position.Y = 0;
+            }
+
+
+
+            //System.Diagnostics.Debug.WriteLine(dt);
+            
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            // Draw a new player
+            spriteBatch.Begin();
+            player.Draw(spriteBatch);
+            ball.Draw(spriteBatch);
+            spriteBatch.End();
+           
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
